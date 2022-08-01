690c7fd54e35ed14a9a30751fc1fd326abb7c51e
Daiianaa <107675013+Daiianaa@users.noreply.github.com>
2022-08-01 05:13:41 -0300

    pase todo a onedrive
    cargar onedrive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,35 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogAPComponent } from './components/log-ap/log-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './component/banner/banner.component';
-
-@NgModule({
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+@NgModule({ 
   declarations: [
     AppComponent,
     HeaderComponent,
     LogAPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,  
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
